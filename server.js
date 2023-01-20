b085ccd455474fa65bfa198f9db5416b518714ca
Gary Gaines <gai18003@byui.edu>
2023-01-20 13:46:22 -0700

    updated server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const bodyParser = require("body-parser");
+const { response } = require("express");
+const express = require ("express");
+const { request } = require("http");
+const app = express();
+
+const port = 3000;
+
+const {v4: uuidv4} = require('uuid'); //universely unique identifier
+
+app.use(bodyParser.json()); //this looks for incoming data
+
+app.get("/", (request, response) => {
+    response.send("Hello Gary");
+}
+);
+
+app.post('/login', (req, res) =>{
+    const loginUser = req.body.userName;
+    const loginPassword = req.body.password; //access the password data in the body
+    console.log('Login username: ' +loginUser);
+    if (loginUser == "gai18003@byui.edu" && loginPassword == "Radar123"){
+        const loginToken = uuidv4();
+        res.send(loginToken)
+
+    }else {
+        res.status(401); //unauthorized
+        res.send("Incorrect password for " + loginUser);
+    }
+});
+
+app.listen(port, () => {
+    console.log("listening");
+});
+
