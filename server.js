d3b86a75e5cb9c27469471f889bc138c37ad1b8a
Gary Gaines <gai18003@byui.edu>
2023-03-29 13:17:20 -0600

    added ingress info to app.yaml

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/rapidsteptest', async(req,res)=>{
 //  -- password default:tT246SyaH8qaCHW6@
 
 const redisClient = Redis.createClient({
-    url: "redis://127.0.0.1:3000"
+    url: "redis://127.0.0.1:6379"
 });
 
 
